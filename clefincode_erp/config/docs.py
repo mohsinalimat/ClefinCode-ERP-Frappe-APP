abc1cf778fe68f88be080cb363145f8ad4149eea
clefincode <77039785+clefincode@users.noreply.github.com>
2024-04-14 14:20:33 +0300

    headline

--- clefincode_erp/config/docs.py
+++ clefincode_erp/config/docs.py
@@ -2,9 +2,9 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/clefincode_erp"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+# source_link = "https://github.com/[org_name]/ClefinCode-ERP-Frappe-APP"
+# headline = "Boost ERPNext with business chat features accessible through mobile apps."
+# sub_heading = "ClefinCode Chat offers a full suite of multimedia messaging capabilities, allowing your team to share pictures, videos, files, and voice clips effortlessly. With an intuitive interface, our chat application facilitates easy adoption, enabling direct messaging or group conversations without the complexity"
 
 def get_context(context):
 	context.brand_html = "ClefinCode ERP"
